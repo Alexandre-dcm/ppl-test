3115da4e84a5e188aaf04594d46c845d67018d69
Alex Dcm <a.dcm33@gmail.com>
2023-12-15 23:20:38 +0100

    Deleted default VueJS init files

---
+++ hospital-fe/EasyCure/src/components/Nav.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="main-nav">
+      <div class="main-nav-logo">
+        <h3>
+          <slot name="heading"></slot>
+        </h3>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+
+  </style>
+  
